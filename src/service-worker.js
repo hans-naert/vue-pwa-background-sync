73097ea3d2d0c34690cc7f21821953575509e727
VIVES-ELICT <hans.naert@vives.be>
2022-05-18 01:24:02 +0200

    added backgroundsync

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,38 +1,40 @@
-import {BackgroundSyncPlugin} from 'workbox-background-sync';
-import {precacheAndRoute} from 'workbox-precaching';
-import {registerRoute} from 'workbox-routing';
-import { NetworkFirst, CacheFirst } from 'workbox-strategies';
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst, CacheFirst, NetworkOnly } from 'workbox-strategies';
 import { ExpirationPlugin } from 'workbox-expiration';
 
 precacheAndRoute(self.__WB_MANIFEST);
-  
-  // Demonstrates using default cache
-  registerRoute(
-    new RegExp('.*\\.(?:js)'),
-    new NetworkFirst(),
-  );
-  
-  // Demonstrates a custom cache name for a route.
-  registerRoute(
-    new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif)'),
-    new CacheFirst({
-      cacheName: 'image-cache',
-      plugins: [
-        new ExpirationPlugin({
-          maxEntries: 3,
-        }),
-      ],
-    }),
-  );
 
-  const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
-    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
-});
-  
-  registerRoute(
-    /\/api\/.*\/*.json/,
-  new NetworkOnly({
+// Demonstrates using default cache
+registerRoute(
+  new RegExp('.*\\.(?:js)'),
+  new NetworkFirst(),
+);
+
+// Demonstrates a custom cache name for a route.
+registerRoute(
+  new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif)'),
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 3,
+      }),
+    ],
+  }),
+);
+
+const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
+    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes),
+    onSync: (options) => {
+      console.log('Sync received', options.queue);
+      options.queue.replayRequests().then(console.log('Replayed requests', options.queue));
+    }
+  });
+
+const networkWithBackgroundSync = new NetworkOnly({
     plugins: [bgSyncPlugin],
-}),
-  'POST'
-  ); 
+  });
+
+registerRoute(/.*/, networkWithBackgroundSync,"POST");  
